b0c18c437727e1ce4748d0c5b98aad341e34a7bf
saiyaraift <saiyara.iftekharuzzaman@gmail.com>
2023-03-24 11:33:00 -0500

    react stuff

---
+++ secoms319_react/initial_app/src/index.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<h1>Hello World!</h1>);
