2011c67e19ca18fc3d4aad716da937920e895ca5
saiyaraift <saiyara.iftekharuzzaman@gmail.com>
2023-05-02 23:54:05 -0500

    adding dataschema.js file

--- team67_final/team67finalbackend/index.js
+++ team67_final/team67finalbackend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const Product = require("./dataSchema.js");
 const app = express();
 
 app.use(express.json());
