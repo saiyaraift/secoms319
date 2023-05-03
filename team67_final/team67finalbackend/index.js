a3e726516543ea67b13e02ad8093c8aaa9682f85
saiyaraift <saiyara.iftekharuzzaman@gmail.com>
2023-05-02 23:48:20 -0500

    connecting backend

---
+++ team67_final/team67finalbackend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/reactdata",
+    {
+        dbName: "reactdata",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+
+
+
+
+const port = process.env.PORT || 4000;
+const host = "localhost";
+app.listen(port, () => {
+    console.log(`App listening at http://%s:%s`, host, port);
+});
